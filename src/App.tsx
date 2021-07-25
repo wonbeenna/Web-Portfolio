ecefea215183b7cddbb144c9ce3e073bbb685ff5
wonbeenna <nwbnwb@naver.com>
2021-07-25 23:18:44 +0900

    start

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./scss/App.scss";
+import Button from "./components/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <Button />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "../scss/Button.scss";
+function Button() {
+  return <button className="Button">Test</button>;
+}
+
+export default Button;
